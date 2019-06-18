b35fd013bf7d66743728a34e9ac046af246e9931
Ricardo Caldeira de Andrade <ricardo.caldeira3@hotmail.com>
2019-06-18 08:32:42 -0300

    Referencias dos Botoes

---
+++ Vira Dose/JogoViewController.swift
@@ -0,0 +1,268 @@
+//
+//  JogoViewController.swift
+//  Vira Dose
+//
+//  Created by Ricardo Caldeira on 18/06/19.
+//  Copyright © 2019 Ricardo Caldeira. All rights reserved.
+//
+
+import UIKit
+
+class JogoViewController: UIViewController {
+    
+    
+    @IBOutlet weak var bt00: UIButton!
+    @IBOutlet weak var bt01: UIButton!
+    @IBOutlet weak var bt02: UIButton!
+    @IBOutlet weak var bt03: UIButton!
+    @IBOutlet weak var bt04: UIButton!
+    @IBOutlet weak var bt05: UIButton!
+    @IBOutlet weak var bt06: UIButton!
+    @IBOutlet weak var bt07: UIButton!
+    @IBOutlet weak var bt08: UIButton!
+    @IBOutlet weak var bt09: UIButton!
+    @IBOutlet weak var bt10: UIButton!
+    @IBOutlet weak var bt11: UIButton!
+    @IBOutlet weak var bt12: UIButton!
+    @IBOutlet weak var bt13: UIButton!
+    @IBOutlet weak var bt14: UIButton!
+    @IBOutlet weak var bt15: UIButton!
+    @IBOutlet weak var bt16: UIButton!
+    @IBOutlet weak var bt17: UIButton!
+    @IBOutlet weak var bt18: UIButton!
+    @IBOutlet weak var bt19: UIButton!
+    @IBOutlet weak var bt20: UIButton!
+    @IBOutlet weak var bt21: UIButton!
+    @IBOutlet weak var bt22: UIButton!
+    @IBOutlet weak var bt23: UIButton!
+    @IBOutlet weak var bt24: UIButton!
+    @IBOutlet weak var bt25: UIButton!
+    @IBOutlet weak var bt26: UIButton!
+    @IBOutlet weak var bt27: UIButton!
+    @IBOutlet weak var bt28: UIButton!
+    @IBOutlet weak var bt29: UIButton!
+    @IBOutlet weak var bt30: UIButton!
+    @IBOutlet weak var bt31: UIButton!
+    @IBOutlet weak var bt32: UIButton!
+    @IBOutlet weak var bt33: UIButton!
+    @IBOutlet weak var bt34: UIButton!
+    @IBOutlet weak var bt35: UIButton!
+    @IBOutlet weak var bt36: UIButton!
+    @IBOutlet weak var bt37: UIButton!
+    @IBOutlet weak var bt38: UIButton!
+    @IBOutlet weak var bt39: UIButton!
+    @IBOutlet weak var bt40: UIButton!
+    @IBOutlet weak var bt41: UIButton!
+    @IBOutlet weak var bt42: UIButton!
+    @IBOutlet weak var bt43: UIButton!
+    @IBOutlet weak var bt44: UIButton!
+    @IBOutlet weak var bt45: UIButton!
+    @IBOutlet weak var bt46: UIButton!
+    @IBOutlet weak var bt47: UIButton!
+    @IBOutlet weak var bt48: UIButton!
+    @IBOutlet weak var bt49: UIButton!
+    @IBOutlet weak var bt50: UIButton!
+    @IBOutlet weak var bt51: UIButton!
+    @IBOutlet weak var bt52: UIButton!
+    @IBOutlet weak var bt53: UIButton!
+    @IBOutlet weak var bt54: UIButton!
+    @IBOutlet weak var bt55: UIButton!
+    @IBOutlet weak var bt56: UIButton!
+    @IBOutlet weak var bt57: UIButton!
+    @IBOutlet weak var bt58: UIButton!
+    @IBOutlet weak var bt59: UIButton!
+    @IBOutlet weak var btComecar: UIButton!
+    
+    @IBAction func btAcao00(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao01(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao02(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao03(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao04(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao05(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao06(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao07(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao08(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao09(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao10(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao11(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao12(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao13(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao14(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao15(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao16(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao17(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao18(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao19(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao20(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao21(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao22(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao23(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao24(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao25(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao26(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao27(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao28(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao29(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao30(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao31(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao32(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao33(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao34(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao35(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao36(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao37(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao38(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao39(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao40(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao41(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao42(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao43(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao44(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao45(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao46(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao47(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao48(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao49(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao50(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao51(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao52(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao53(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao54(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao55(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao56(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao57(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao58(_ sender: Any) {
+    }
+    
+    @IBAction func btAcao59(_ sender: Any) {
+    }
+    
+    @IBAction func btAcaoComecar(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    
+
+
+}
